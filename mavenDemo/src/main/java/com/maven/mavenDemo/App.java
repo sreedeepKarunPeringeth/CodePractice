7fdace7b25ea319105fd2b9480aac9e78fb63d25
sreedeep <speringeth@sapient.com>
2018-12-10 13:31:17 +0530

    mavenDemo

---
+++ mavenDemo/src/main/java/com/maven/mavenDemo/App.java
@@ -0,0 +1,19 @@
+package com.maven.mavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void showMessage(){
+		System.out.println("showMessage() method");
+	}
+	
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.showMessage();
+    }
+}
